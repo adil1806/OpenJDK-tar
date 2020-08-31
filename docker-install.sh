330de05d00e545bf607ff188297d552d99469f8c
KHAN ADIL <36574088+adil1806@users.noreply.github.com>
2020-08-31 15:24:40 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -15,4 +15,4 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
